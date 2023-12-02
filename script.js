619c12d5b42af0d74f52b57fe5fe1a57a3334d58
Sammy150150 <138682567+Sammy150150@users.noreply.github.com>
2023-12-02 20:14:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function anImportantMessage(){
+    alert("Hello, my friend");
+    alert("I am here to tell you");
+    alert("That you are gay");
+    alert("You will die a virgin");
+    alert("And you are ugly as shit");
+    alert("Thank you for your time :)");
+}
+anImportantMessage()
